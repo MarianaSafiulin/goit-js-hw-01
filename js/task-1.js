e1468c94b2722b892061d0ff69f0231dbfd9b96d
MarianaSafiulin <143293934+MarianaSafiulin@users.noreply.github.com>
2024-02-20 12:18:13 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,8 @@
+
+
+
 function makeTransaction(quantity, pricePerDroid) {
-  let totalPrice = quantity * pricePerDroid;
+  const totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
